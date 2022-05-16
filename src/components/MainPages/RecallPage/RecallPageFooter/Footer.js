c264e626320e81682f85b0483fbc49538faad015
Luan Severo <luansevero@icloud.com>
2022-05-16 16:25:51 -0300

    Hotfix Layout

--- src/components/MainPages/RecallPage/RecallPageFooter/Footer.js
+++ src/components/MainPages/RecallPage/RecallPageFooter/Footer.js
@@ -5,25 +5,31 @@ import BtnReset from "./FooterContents/FooterBtnReset";
 export default function Footer({size, qAnswered, answersIcons, recallLevel, setChangePage}){
      if(size === qAnswered){
         return(
-            <div className="footer">
+            <div className="footer margin">
+                <div className="content">
                 <Result answersIcons={answersIcons} size={size} qAnswered={qAnswered} recallLevel={recallLevel} />
                 <h2 className="progress margin">{qAnswered}/{size} CONCLUÍDOS</h2>
                 <Icons answersIcons={answersIcons}/>
                 <BtnReset setChangePage={setChangePage} />
+                </div>
             </div>
         )
     }
     if(qAnswered === 0){
         return(
             <div className="footer">
-                <h2 className="progress">{qAnswered}/{size} CONCLUÍDOS</h2>
+                <div className="content">
+                    <h2 className="progress">{qAnswered}/{size} CONCLUÍDOS</h2>
+                </div>
             </div>
         )
     } else {
         return(
             <div className="footer">
-                <h2 className="progress margin">{qAnswered}/{size} CONCLUÍDOS</h2>
-                <Icons answersIcons={answersIcons}/>
+                <div className="content">
+                    <h2 className="progress margin">{qAnswered}/{size} CONCLUÍDOS</h2>
+                    <Icons answersIcons={answersIcons}/>
+                </div>
             </div>
         )
     }
